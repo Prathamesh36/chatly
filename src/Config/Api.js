fab606b2ebfcf3fadc680154eb80ec28eaf4c4ce
Prathamesh <pdpatil1006@gmail.com>
2024-05-05 21:07:58 +0530

    config change

--- src/Config/Api.js
+++ src/Config/Api.js
@@ -1,3 +1,3 @@
 // export const BASE_URL="http://localhost:5454/api";
 
-export const BASE_URL="chat-application-backend.railway.internal";
+export const BASE_URL="chat-application-backend-production-adb4.up.railway.app";
