0600083e10ca868649c026527b671853b13068fe
Prathamesh36 <65437511+Prathamesh36@users.noreply.github.com>
2024-05-05 20:06:53 +0530

    Delete src directory

--- src/Redux/Empty/Action.js
+++
@@ -1,6 +0,0 @@
-export const setFieldToNull = (type) => {
-    return dispatch => {
-      // Dispatch a regular action to update the field to null
-      dispatch({ type: type, payload: null });
-    };
-  };
